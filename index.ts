68ca6069e25f46302d350add13ed30e247f809aa
iCalculated <fufa0001@gmail.com>
2021-10-12 18:53:52 -0500

    chat message emit

--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 const express = require('express');
 const http = require('http')
-
+const { Server } = require("socket.io")
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    });
+    socket.on('chat message', (msg) => {
+        console.log('received: ' + msg)
+    });
 });
 
 server.listen(3000, () => {
